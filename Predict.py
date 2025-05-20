4a4710663dc774a537aadc9fe4dcf6cc68d79679
MuhammadUmarAbdulAziz <abdulazizumar01@gmail.com>
2025-05-21 02:07:19 +0700

    Add files via upload

--- Predict.py
+++ Predict.py
@@ -1,13 +1,21 @@
-from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_iris
 import joblib
 
-# Contoh dataset Iris
-data = load_iris()
-X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2)
-model = RandomForestClassifier()
-model.fit(X_train, X_train)
+# Load dataset
+df = pd.read_csv('used_car_cleaned.csv')
+
+# Pisahkan fitur dan target
+X = df.drop('Price', axis=1)
+y = df['Price']
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Latih model
+model = RandomForestRegressor(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
 
 # Simpan model ke file
 joblib.dump(model, 'used_car_price_model_mae.pkl')
