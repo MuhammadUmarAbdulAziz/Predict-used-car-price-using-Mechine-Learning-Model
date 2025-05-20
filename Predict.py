625237de2a7a926396fed58380163f6cdd189e26
MuhammadUmarAbdulAziz <abdulazizumar01@gmail.com>
2025-05-21 02:11:24 +0700

    Add files via upload

--- Predict.py
+++ Predict.py
@@ -1,21 +1,11 @@
-import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
-import joblib
+from pathlib import Path
+import pickle
 
-# Load dataset
-df = pd.read_csv('used_cars_cleaned.csv')
-
-# Pisahkan fitur dan target
-X = df.drop('Price', axis=1)
-y = df['Price']
-
-# Split data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Latih model
-model = RandomForestRegressor(n_estimators=100, random_state=42)
-model.fit(X_train, y_train)
-
-# Simpan model ke file
-joblib.dump(model, 'used_car_price_model_mae.pkl')
+def predict(df):
+    # Load the model and scaler from the saved file
+    # Load model
+    file_name =str(Path(__file__).parents[1] / 'used_car_price_model_mae.pkl')
+    model = pickle.load(open(file_name, 'rb'))
+    
+    pred = model.predict(df)
+    return pred
